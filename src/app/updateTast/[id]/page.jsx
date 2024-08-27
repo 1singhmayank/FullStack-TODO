b777bc38d0a1127522127450e67f9c282c3bf4ab
1singhmayank <133634405+1singhmayank@users.noreply.github.com>
2024-08-27 14:15:54 +0530

    Update page.jsx

--- src/app/updateTast/[id]/page.jsx
+++ src/app/updateTast/[id]/page.jsx
@@ -1,42 +1,44 @@
-import React from 'react'
-import style from '../[id]/update.module.css'
-import AddUser, { UpdateUser } from '../../lib/actions'
-import FetchUser from '../../lib/fetch'
-import FetchUserId from '../../lib/fetch'
-const UpdateUsers = async ({params}) => {
+import React from 'react';
+import style from '../[id]/update.module.css';
+import { UpdateUser } from '../../lib/actions';
+import FetchUserId, { FetchUser } from '../../lib/fetch';
 
-  const {id}  = params
-  const users = await FetchUserId(id)
+// This function generates the static paths for the dynamic route
+export async function generateStaticParams() {
+  const users = await FetchUser();
+
+  return users.map((user) => ({
+    id: user._id.toString(), // Ensure that id is a string
+  }));
+}
+
+const UpdateUsers = async ({ params }) => {
+  const { id } = params;
+  const user = await FetchUserId(id);
 
   return (
     <div className={style.container}>
-      <h2 className='ml-[17%] mb-2 font-bold '>Update Task Form</h2>
-        <form action={UpdateUser} className={style.form}>
-          <input type='hidden' name='id' value={id} />
-            <input
-              type="text"
-              placeholder={users.title}
-              name='title'
-              required
-              className={style.formInput}
-            />
-
-            <textarea
-              name="description"
-              cols={5}
-              rows={5}
-
-              placeholder={users.description}
-              className={style.formInput}
-            ></textarea>
-
-            <button type='submit' className={style.formButton}>Update</button>
-        </form>
+      <h2 className="ml-[17%] mb-2 font-bold">Update Task Form</h2>
+      <form action={UpdateUser} method="POST" className={style.form}>
+        <input type="hidden" name="id" value={id} />
+        <input
+          type="text"
+          defaultValue={user.title} // Prefill input with the title
+          name="title"
+          required
+          className={style.formInput}
+        />
+        <textarea
+          name="description"
+          cols={5}
+          rows={5}
+          defaultValue={user.description} // Prefill textarea with the description
+          className={style.formInput}
+        ></textarea>
+        <button type="submit" className={style.formButton}>Update</button>
+      </form>
     </div>
-
-
-
-  )
-}
+  );
+};
 
 export default UpdateUsers;
